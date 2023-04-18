418125a9fefda2feab254ef70663b75e0992dde9
hyun3640 <uranus165@gmail.com>
2023-04-18 18:10:50 +0900

    4/18

--- src/main/java/com/springbook/view/common/CommonExceptionHandler.java
+++ src/main/java/com/springbook/view/common/CommonExceptionHandler.java
@@ -15,4 +15,19 @@ public class CommonExceptionHandler {
 		return mav;
 	}
 	
+	@ExceptionHandler(NullPointerException.class)
+	public ModelAndView handleNullPointerException(Exception e) {
+		ModelAndView mav= new ModelAndView();
+		mav.addObject("exception",e);
+		mav.setViewName("/common/nullPointerError.jsp");
+		return mav;
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleException(Exception e) {
+		ModelAndView mav= new ModelAndView();
+		mav.addObject("exception",e);
+		mav.setViewName("/common/Error.jsp");
+		return mav;
+	}
 }
